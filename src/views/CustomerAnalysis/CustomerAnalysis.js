22b275c401156e6e83d67023aa2d4654eb26686c
Joham Jaider Morales Franco <johamfranco318@gmail.com>
2023-11-16 13:10:12 -0500

    Clientes por genero

--- src/views/CustomerAnalysis/CustomerAnalysis.js
+++ src/views/CustomerAnalysis/CustomerAnalysis.js
@@ -4,12 +4,14 @@ import CustomerCounter from "./components/CustomerCounter";
 import CustomersPerYear from "./components/CustomersPerYear";
 import ClientsByCity from "./components/ClientsByCity";
 import ClientByAge from "./components/ClientByAge";
+import ClientsByGender from "./components/ClientsByGender";
 
 const CustomerAnalysis = () => {
     return(
         <Box>
             <Grid container spacing={0}>
                 <CustomerCounter/>
+                <ClientsByGender/>
                 <CustomersPerYear/>
                 <ClientsByCity/>
                 <ClientByAge/>
---
+++ src/views/CustomerAnalysis/components/ClientsByGender.js
@@ -0,0 +1,88 @@
+import React, {useEffect, useRef} from "react";
+import * as echarts from "echarts";
+import {Box, Card, CardContent, Typography} from "@mui/material";
+
+const ClientsByGender = () => {
+
+    const chartRef = useRef(null);
+
+    useEffect(() => {
+        let myChart = null;
+
+        if (chartRef.current) {
+            myChart = echarts.init(chartRef.current);
+
+            const option = {  tooltip: {
+                    trigger: 'item'
+                },
+                legend: {
+                    top: '5%',
+                    left: 'center'
+                },
+                series: [
+                    {
+                        name: 'Access From',
+                        type: 'pie',
+                        radius: ['40%', '70%'],
+                        avoidLabelOverlap: false,
+                        itemStyle: {
+                            borderRadius: 10,
+                            borderColor: '#fff',
+                            borderWidth: 2
+                        },
+                        label: {
+                            show: false,
+                            position: 'center'
+                        },
+                        emphasis: {
+                            label: {
+                                show: true,
+                                fontSize: 40,
+                                fontWeight: 'bold'
+                            }
+                        },
+                        labelLine: {
+                            show: false
+                        },
+                        data: [
+                            { value: 1048, name: 'Search Engine' },
+                            { value: 735, name: 'Direct' },
+                            { value: 580, name: 'Email' },
+                            { value: 484, name: 'Union Ads' },
+                            { value: 300, name: 'Video Ads' }
+                        ]
+                    }
+                ]
+            };
+
+            myChart.setOption(option);
+        }
+
+        // Limpiar el gráfico al desmontar el componente
+        return () => {
+            if (myChart) {
+                myChart.dispose();
+            }
+        };
+    }, []);
+
+    return (
+        <Card>
+            <CardContent>
+                <Box >
+                    <Typography
+                        variant="h2"
+                        sx={{
+                            marginBottom: "0",
+                            paddingBottom: "20px",
+                        }}
+
+                    >Clientes por genero</Typography>
+                    <div ref={chartRef} style={{ width: '600px', height: '600px' }} />
+                </Box>
+            </CardContent>
+        </Card>
+    );
+}
+
+export default ClientsByGender;
